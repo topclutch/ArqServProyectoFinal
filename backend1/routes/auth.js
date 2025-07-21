0a84e038262b593fe85151cf7b4deb14c24e696f
JosefaFranc <164115559+YuliFrancisco8@users.noreply.github.com>
2025-07-20 22:06:52 -0600

    Update auth.js

--- backend1/routes/auth.js
+++ backend1/routes/auth.js
@@ -1,1 +1,14 @@
 
+import express from 'express'
+import { login, register } from '../controllers/authController.js'
+import { validateUser, validateLogin } from '../validators/userValidator.js'
+// Agrega esta importación al principio del archivo
+import { authenticateToken } from '../middleware/auth.js';
+import { getCurrentUser } from '../controllers/authController.js';
+const router = express.Router()
+
+router.post('/login', validateLogin, login)
+router.post('/register', validateUser, register)
+router.get('/me', authenticateToken, getCurrentUser); // Nuevo endpoint
+
+export default router
